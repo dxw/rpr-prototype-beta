4bcb65a63fe02a7c9169ae7ea9f842f51205afb3
Ming Chan <mingosoft@gmail.com>
2022-02-08 12:26:37 +0000

    updated filter behaviour

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,12 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+  if (window.screen.width < 480) {
+    document.getElementsByClassName('rpr-listing__filters-container')[0].removeAttribute("open");
+  };
+
+  if (window.screen.width >= 480) {
+    document.getElementsByClassName('rpr-listing__filters-container')[0].addAttribute("open");
+  };
 })
